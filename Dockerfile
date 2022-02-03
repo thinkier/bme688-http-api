44a3e503f5b87dc4aceb5ab967683dbc8a77f59a
thinkier <26728734+thinkier@users.noreply.github.com>
2022-02-03 22:42:06 +1100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.9-buster
+RUN pip3 install --upgrade pip
+RUN pip3 install bme680 flask
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+COPY . .
+
+CMD python3 main.py
---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from flask import Flask, jsonify
+from bme680 import *
+from time import sleep
+
+sensor = BME680(I2C_ADDR_PRIMARY)
+
+sensor.set_humidity_oversample(OS_2X)
+sensor.set_pressure_oversample(OS_2X)
+sensor.set_temperature_oversample(OS_2X)
+sensor.set_filter(FILTER_SIZE_3)
+
+
+app = Flask(__name__)
+
+@app.route("/report.json")
+def report_json():
+    sensor.get_sensor_data()
+    data = {
+        'temp': sensor.data.temperature,
+        'pressure': sensor.data.pressure,
+        'humidity': sensor.data.humidity
+    }
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5555')
