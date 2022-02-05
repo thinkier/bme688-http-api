1df68ef930afa87c05f6812c27c1eb37a2b464df
thinkier <26728734+thinkier@users.noreply.github.com>
2022-02-05 13:31:41 +1100

    return raw data isntead, just round sIAQ

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ def report_json():
         'humidity': bsec_data['humidity'],
         'pressure': bsec_data['raw_pressure'],
         'air_quality': air_quality_homekit,
-        'co2_ppm': round(bsec_data['co2_equivalent']),
-        'voc_ppm': round(bsec_data['breath_voc_equivalent'])
+        'co2_ppm': bsec_data['co2_equivalent'],
+        'voc_ppm': bsec_data['breath_voc_equivalent']
     }
 
     return jsonify(data)
