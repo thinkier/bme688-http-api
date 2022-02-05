bebd3aecdbdbf5278280d149219c159355ff5881
thinkier <26728734+thinkier@users.noreply.github.com>
2022-02-05 13:15:29 +1100

    it's a dict and not an obj

--- main.py
+++ main.py
@@ -37,18 +37,18 @@ def report_json():
 
     # Output range is 0-500, I want to turn it to 1-5 with consideration for Table 6 at
     # https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme688-ds000.pdf
-    air_quality_homekit = (bsec_data.static_iaq / 62.5) + 1
+    air_quality_homekit = (bsec_data['static_iaq'] / 62.5) + 1
 
     if air_quality_homekit > 5:
         air_quality_homekit = 5
 
     data = {
-        'temperature': bsec_data.temperature,
-        'humidity': bsec_data.humidity,
-        'pressure': bsec_data.raw_pressure,
+        'temperature': bsec_data['temperature'],
+        'humidity': bsec_data['humidity'],
+        'pressure': bsec_data['raw_pressure'],
         'air_quality': air_quality_homekit,
-        'co2_ppm': bsec_data.co2_equivalent,
-        'voc_ppm': bsec_data.breath_voc_equivalent
+        'co2_ppm': bsec_data['co2_equivalent'],
+        'voc_ppm': bsec_data['breath_voc_equivalent']
     }
 
     return jsonify(data)
