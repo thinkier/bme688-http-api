07f8c296dbd2c5253babd2c9717aa209a3b24196
thinkier <26728734+thinkier@users.noreply.github.com>
2022-10-31 19:49:34 +1100

    cross calibrated the compensated temp and hum and it's crap

--- main.py
+++ main.py
@@ -43,13 +43,14 @@ def report_json():
         air_quality_homekit = 5
 
     data = {
-        'temperature': bsec_data['temperature'],
-        'humidity': bsec_data['humidity'],
+        'temperature': bsec_data['raw_temperature'],
+        'humidity': bsec_data['raw_humidity'],
         'pressure': bsec_data['raw_pressure'],
         'air_quality': air_quality_homekit,
-        'co2_ppm': bsec_data['co2_equivalent'],
         'voc_ppm': bsec_data['breath_voc_equivalent']
     }
+
+    print(bsec_data)
 
     return jsonify(data)
 
